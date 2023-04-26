92f9d7177cce8b6171f2264ca75b39e07bea2d8a
TthKgeMitR <takamura.tomoki387@mail.kyutech.jp>
2023-04-27 07:41:41 +0900

    first commit.

---
+++ setup_arm64.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PWD="pass"
+
+echo $PWD | sudo -S apt install -y terminator
+echo $PWD | sudo -S apt install -y openssh-server
+echo $PWD | sudo -S apt install -y neovim
+echo $PWD | sudo -S apt install -y open-vm-tools-desktop
+
+LANG=C xdg-user-dirs-gtk-update
+
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh
+bash Miniforge3-Linux-aarch64.sh
+
+mkdir -p ~/.congig/nvim
+echo "set number" >> ~/.congig/nvim/init.vim
+echo "set smartindent" >> ~/.congig/nvim/init.vim
+echo "" >> ~/.congig/nvim/init.vim
+echo "set expandtab" >> ~/.congig/nvim/init.vim
+echo "set shiftwidth=4" >> ~/.congig/nvim/init.vim
+echo "set softtabstop=4" >> ~/.congig/nvim/init.vim
+echo "set tabstop=4" >> ~/.congig/nvim/init.vim
+echo "" >> ~/.congig/nvim/init.vim
+echo "set imdisable" >> ~/.congig/nvim/init.vim
+echo "" >> ~/.congig/nvim/init.vim
+echo "syntax enable" >> ~/.congig/nvim/init.vim
+
+echo "" >> ~/.bashrc
+echo "alias nv='nvim'" >> ~/.bashrc
+
+echo "" >> ~/.bashrc
+echo "export PROMPT_COMMAND='history -a; history -c; history -r; $PROMPT_COMMAND'" >> ~/.bashrc
+echo "shopt -u histappend" >> ~/.bashrc
+
+source ~/.bashrc
